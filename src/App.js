39111052796d4ec4594bde9bb944af64468e66b4
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-19 14:26:20 +0300

    Added Add survey component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import "./App.css";
 import Surveys from "./components/Surveys";
+import AddSurvey from "./components/AddSurvey";
 
 function App() {
    // Initialize State
   const [surveys, setSurveys] = useState([]);
   const [showAddSurvey, setShowAddSurvey] = useState(false);
 
+  const [questions, setQuestions] = useState([]);
+  const [showAddQuestion, setShowAddQuestion] = useState(false);
+
+  const [options, setOptions] = useState([]);
+  const [showAddOption, setShowAddOption] = useState(false);
+
+  const [question_count, setQCount] = useState(1);
+  const [option_count, setOCount] = useState(1);
 
   // Initialize all surveys into state from backend at component load
   useEffect(() => {
@@ -16,7 +25,6 @@ function App() {
       const surveysFromServer = await fetchSurveys();
       console.log(surveysFromServer);
       setSurveys(surveysFromServer);
-      console.log(surveys);
     };
     getSurveys();
   }, []);
@@ -34,7 +42,34 @@ function App() {
     }
   };
 
+  //Adding a Survey
+  const addSurvey = async (survey) => {
+    const res = await fetch("http://127.0.0.1:8000/api/v1/admin/add_survey", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(
+       { survey,
+        question_count:question_count,
+        option_count:option_count})
+    });
+    const data = await res.json();
+    console.log(JSON.stringify(
+       { survey,
+        question_count:question_count,
+        option_count:option_count}));
+    setSurveys([...surveys, data]);
+  };
 
+
+  //Fetch one survey from backend
+  const fetchSurvey = async (id) => {
+    const res = await fetch(`http://127.0.0.1:8000/api/v1/user/get_survey?survey_id=${id}`);
+    const data = await res.json();
+    return data;
+  };
 
     return(
      <BrowserRouter><div className = "container">
@@ -60,6 +95,29 @@ function App() {
                 )}
               </>}
               ></Route>
+              <Route
+                path="/AddSurvey"
+                element={
+                <AddSurvey
+                onAdd={addSurvey}
+                  question_count={question_count}
+                  option_count={option_count}
+                  onAddQuestion={(e)=>{
+                  e.preventDefault();
+                  setQCount(question_count + 1);
+                  // console.log(question_count);
+                }
+
+              }
+                  showAddQuestion={showAddQuestion}
+                  onAddOption={(e)=>{
+                  e.preventDefault();
+                  setOCount(option_count + 1);
+                  //setShowAddOption(!showAddOption);
+                  console.log(option_count);
+                }}
+                  showAddOption={showAddOption}/>}>
+              </Route>
         </Routes> </div>
       </BrowserRouter>);
 }
---
+++ src/components/AddSurvey.js
@@ -0,0 +1,100 @@
+import React, {useState} from "react";
+import Button from "./Button";
+
+const AddSurvey = ({
+    onAdd,
+    question_count,
+    option_count,
+    onAddQuestion,
+    showAddQuestion,
+    showAddOption,
+    onAddOption
+}) => {
+    const [title,
+        setTitle] = useState("");
+    const [questions,
+        setQuestions] = useState([]);
+    const [options,
+        setOptions] = useState([]);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (!title || !questions || !options) {
+            alert("Please fill all the fields!");
+            return;
+        }
+    onAdd({title, questions, options});
+    setTitle("");
+    setQuestions([]);
+    setOptions([]);};
+    return (
+        <form className="add-form" onSubmit={onSubmit}>
+            <div className="form-control">
+                <label>Survey Title</label>
+                <input
+                    type="text"
+                    placeholder="Add Survey"
+                    onChange={(e) => {
+                    setTitle(e.target.value);
+                }}/>
+            </div>
+            <Button
+                color={showAddQuestion
+                ? "red"
+                : "purple"}
+                text={showAddQuestion
+                ? "Done"
+                : "Add Question"}
+                onClick={onAddQuestion}/> {Array
+                .from(Array(question_count))
+                .map((c, index) => {
+                    return (
+                        <div className="form-control">
+                            <input
+                                key={c}
+                                id
+                                ={index}
+                                type="text"
+                                placeholder={"Text field"}
+                                onChange={(e) => {
+                                setQuestions([
+                                    ...questions,
+                                      e.target.value
+                                ]);
+                            }}/>
+                            <br/>
+                        </div >
+                    );
+                })}
+            <Button
+                color={showAddOption
+                ? "red"
+                : "purple"}
+                text={showAddOption
+                ? "Done"
+                : "Add Choice"}
+                onClick={onAddOption}/>{Array
+                .from(Array(option_count))
+                .map((c, index) => {
+                    return (
+                        <div className="form-control">
+                            <input
+                                key={c}
+                                id
+                                ={index}
+                                type="text"
+                                placeholder={"Text field"}
+                                onChange={(e) => {
+                                setOptions([
+                                    ...options,
+                                    e.target.value
+                                ]);
+                            }}/>
+                            <br/>
+                        </div >
+                    );
+                })}
+            <input type={"submit"} value="Save Survey" className="btn btn-block"/>
+        </form>
+    );
+};
+export default AddSurvey;
