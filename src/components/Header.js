9d9be69deb6aed00f990ab56317eb841f776e33d
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-19 14:23:58 +0300

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "./Button";
+import {Link} from "react-router-dom";
+
+const Header = ({onAddSurvey, showAddSurvey, title}) => {
+    return (
+        <header className="header">
+            <h1>
+                {title}
+            </h1>
+            <Link to="/AddSurvey">
+                <Button
+                    color={showAddSurvey
+                    ? "red"
+                    : "purple"}
+                    text={showAddSurvey
+                    ? "Done"
+                    : "Add Survey"}
+                    onClick={onAddSurvey}/>
+            </Link>
+        </header>
+    );
+};
+
+export default Header;
