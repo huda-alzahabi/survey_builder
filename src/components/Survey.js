e4fe22afb42cd71df4eaa485efd6c2de43218623
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-20 17:07:49 +0300

    Changed className in survey

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -4,10 +4,14 @@ import React, { useState } from "react";
 import { FaArrowCircleRight } from "react-icons/fa";
 
 const Survey = ({ survey }) => {
-    return ( <div className = "task" >
-              <h3 > { survey.title } <FaArrowCircleRight/></h3>
-              </div>
-            );
+  return (
+    <div className="survey">
+      <h3>
+        {" "}
+        {survey.title} <FaArrowCircleRight />
+      </h3>
+    </div>
+  );
 };
 
 export default Survey;
